6a8cbec368e7df51fc78b0ee891879de0fe528be
ValentinLHB <valentin.lhb@gmail.com>
2020-12-01 14:43:11 +0100

    added position, price, profit count, pnl tracking

--- algorithms/crossover_algo.py
+++ algorithms/crossover_algo.py
@@ -45,24 +45,96 @@ df = df.iloc[10:]
 
 n = 0
 
-while n < len(df):
-    if df.iloc[n, 6] > df.iloc[n, 7]: # if sma_5 > sma_10
-        print(
-            df.index[n],
-            ': FAST > SLOW / BUY! ', 
-            round(df.iloc[n, 6]), 
-            ' > ', 
-            round(df.iloc[n, 7])
-        )
-    else: # sma_5 < sma_10
-        print(
-            df.index[n],
-            ': FAST < SLOW / SELL! ',
-            round(df.iloc[n, 6]),
-            ' < ',
-            round(df.iloc[n, 7])
-        )
+# while n < len(df):
+#     if df.iloc[n, 6] > df.iloc[n, 7]: # if sma_5 > sma_10
+#         print(
+#             df.index[n],
+#             ': FAST > SLOW / BUY! ', 
+#             round(df.iloc[n, 6]), 
+#             ' > ', 
+#             round(df.iloc[n, 7])
+#         )
+#     else: # sma_5 < sma_10
+#         print(
+#             df.index[n],
+#             ': FAST < SLOW / SELL! ',
+#             round(df.iloc[n, 6]),
+#             ' < ',
+#             round(df.iloc[n, 7])
+#         )
     
-    n = n + 1
+#     n = n + 1
 
 # TODO: Strategy 2: If sma-5 CROSSES OVER sma-10: Buy / vice-versa
+# Add position tracker (0 or 1)
+# Add profit counter
+# Add order price-tracking
+# Add PnL calculation
+
+position = 0
+profit_counter = 0
+orders = []
+pnl = []
+
+while n < len(df):
+
+    buy_sell = False
+    
+    if position == 0 and df.iloc[n, 6] > df.iloc[n, 7]:
+        print(
+            df.index[n],
+            'BUY    | sma_5: {} | sma_10: {} | Price: {}'.format(
+                round(
+                    df.iloc[n, 6]
+                ),
+                round(
+                    df.iloc[n, 7]
+                ),
+                df.iloc[n, 3]
+            )
+        )
+        position = 1
+        orders.append(df.iloc[n, 3])
+        buy_sell = False
+    
+    elif position == 1 and df.iloc[n, 6] < df.iloc[n, 7]:
+        print(
+            df.index[n],
+            'SELL   | sma_5: {} | sma_10: {} | Price: {}'.format(
+                round(
+                    df.iloc[n, 6]
+                ),
+                round(
+                    df.iloc[n, 7]
+                ),
+                df.iloc[n, 3]
+            )
+        )
+        position = 0
+        orders.append(df.iloc[n, 3])
+        buy_sell = True
+
+        profit = orders[-2] - orders[-1] # order[-1] sell price, order[-2] is buy price
+        pnl.append(profit)
+
+        if profit > 0:
+            profit_counter += 1
+
+    else:
+        pass
+    
+    n += 1
+
+print('Orders passed:   ', len(orders))
+print('Profit counter:  ', profit_counter)
+print(
+    'Win Rate:   ',
+    round(
+        profit_counter / len(orders) * 100, 2
+    ),
+    '%'
+)
+print(
+    'PnL:       {}'.format('$'), 
+    round(sum(pnl), 2)
+)
