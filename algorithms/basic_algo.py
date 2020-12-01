c58f270d33a03d0a61730d14c77a615d7e085b32
ValentinLHB <valentin.lhb@gmail.com>
2020-12-01 10:46:46 +0100

    basic if / else algo

---
+++ algorithms/basic_algo.py
@@ -0,0 +1,15 @@
+import numpy
+
+# Data
+
+array = numpy.random.rand(500)
+data = array.tolist()
+
+# Problem: we want to know if the value in the list is > 0.5 or not
+
+for x in data:
+    if x > 0.5:
+        print('x > 0.5')
+    
+    else:
+        print('x < 0.5')
